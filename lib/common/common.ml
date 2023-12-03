fd9ab0f4d37e8429d0078ea6a9683ed78559451c
DallogFheir <dallog.fheir@gmail.com>
2023-12-03 16:30:37 +0100

    day 3

--- lib/common/common.ml
+++ lib/common/common.ml
@@ -4,3 +4,25 @@ let file_to_list path =
     try input_line file :: acc |> read_file with End_of_file -> acc
   in
   read_file [] |> List.rev
+
+let zip_with_index lst = List.mapi (fun idx el -> (idx, el)) lst
+
+let fold_lefti fn acc lst =
+  List.fold_left (fun acc (idx, el) -> fn acc idx el) acc (zip_with_index lst)
+
+let get_neighbor_idxs (row_idx, col_idx) row_length col_length =
+  let addends = List.init 3 (fun idx -> idx - 1)
+  and is_valid_idx idx bound = idx >= 0 && idx < bound in
+  addends
+  |> List.map (fun row_addend ->
+         addends
+         |> List.filter_map (fun col_addend ->
+                let row_coord = row_idx + row_addend
+                and col_coord = col_idx + col_addend in
+                if
+                  (not (row_addend = 0 && col_addend = 0))
+                  && is_valid_idx row_coord row_length
+                  && is_valid_idx col_coord col_length
+                then Some (row_coord, col_coord)
+                else None ) )
+  |> List.flatten
---
+++ lib/day03/day03.ml
@@ -0,0 +1,119 @@
+let extract_nums_and_symbols lines =
+  let num_of_chars = List.length lines * (List.hd lines |> String.length) in
+  let symbol_coords = Hashtbl.create num_of_chars in
+  let nums =
+    lines
+    |> Common.fold_lefti
+         (fun acc line_idx line ->
+           line |> String.to_seq |> List.of_seq
+           |> Common.fold_lefti
+                (fun ((nums, numAcc) as acc) symbol_idx symbol ->
+                  match (symbol, String.length numAcc) with
+                  | '0' .. '9', _ ->
+                      (nums, numAcc ^ String.make 1 symbol)
+                  | '.', 0 ->
+                      acc
+                  | '.', _ ->
+                      ( (numAcc, (line_idx, symbol_idx - String.length numAcc))
+                        :: nums
+                      , "" )
+                  | _, 0 ->
+                      Hashtbl.add symbol_coords (line_idx, symbol_idx) symbol ;
+                      acc
+                  | _, _ ->
+                      Hashtbl.add symbol_coords (line_idx, symbol_idx) symbol ;
+                      ( (numAcc, (line_idx, symbol_idx - String.length numAcc))
+                        :: nums
+                      , "" ) )
+                (acc, "")
+           |> fun (nums, numAcc) ->
+           match numAcc with
+           | "" ->
+               nums
+           | _ ->
+               (numAcc, (line_idx, String.length line - String.length numAcc))
+               :: nums )
+         []
+  in
+  (nums, symbol_coords)
+
+let part_1_aux path =
+  let lines = Common.file_to_list path in
+  let nums, symbols = extract_nums_and_symbols lines
+  and num_of_rows = List.length lines
+  and num_of_cols = List.hd lines |> String.length in
+  List.fold_left
+    (fun sum (num_str, (row_coord, start_col_coord)) ->
+      sum
+      +
+      if
+        List.init (String.length num_str) (fun idx -> idx + start_col_coord)
+        |> List.exists (fun col_coord ->
+               Common.get_neighbor_idxs (row_coord, col_coord) num_of_rows
+                 num_of_cols
+               |> List.exists (fun coord -> Hashtbl.mem symbols coord) )
+      then int_of_string num_str
+      else 0 )
+    0 nums
+
+let part_2_aux path =
+  let lines = Common.file_to_list path in
+  let nums, symbols = extract_nums_and_symbols lines
+  and num_of_rows = List.length lines
+  and num_of_cols = List.hd lines |> String.length in
+  let gear_to_nums = Hashtbl.create (num_of_rows * num_of_cols) in
+  List.iter
+    (fun (num_str, (row_coord, start_col_coord)) ->
+      let _ =
+        List.init (String.length num_str) (fun idx -> idx + start_col_coord)
+        |> List.exists (fun col_coord ->
+               Common.get_neighbor_idxs (row_coord, col_coord) num_of_rows
+                 num_of_cols
+               |> List.exists (fun coord ->
+                      if Hashtbl.find_opt symbols coord = Some '*' then (
+                        let num = int_of_string num_str in
+                        ( match Hashtbl.find_opt gear_to_nums coord with
+                        | Some num_lst ->
+                            Hashtbl.replace gear_to_nums coord (num :: num_lst)
+                        | None ->
+                            Hashtbl.add gear_to_nums coord [num] ) ;
+                        true )
+                      else false ) )
+      in
+      () )
+    nums ;
+  Hashtbl.to_seq_values gear_to_nums
+  |> List.of_seq
+  |> List.filter (fun num_lst -> List.length num_lst = 2)
+  |> List.fold_left
+       (fun sum num_lst ->
+         match num_lst with
+         | [num_1; num_2] ->
+             sum + (num_1 * num_2)
+         | _ ->
+             failwith "This should not happen." )
+       0
+
+let test_1 () = part_1_aux "lib/day03/test.txt"
+
+let part_1 () = part_1_aux "lib/day03/input.txt"
+
+let test_2 () = part_2_aux "lib/day03/test.txt"
+
+let part_2 () = part_2_aux "lib/day03/input.txt"
+
+let test () =
+  print_string "Test part 1: " ;
+  test_1 () |> print_int ;
+  print_newline () ;
+  print_string "Test part 2: " ;
+  test_2 () |> print_int ;
+  print_newline ()
+
+let solution () =
+  print_string "Part 1: " ;
+  part_1 () |> print_int ;
+  print_newline () ;
+  print_string "Part 2: " ;
+  part_2 () |> print_int ;
+  print_newline ()
