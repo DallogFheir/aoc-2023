323ba845f2bdeb1fb5c5e88e921598090bd72a67
DallogFheir <dallog.fheir@gmail.com>
2023-12-04 08:08:26 +0100

    day 4

--- lib/common/common.ml
+++ lib/common/common.ml
@@ -10,6 +10,15 @@ let zip_with_index lst = List.mapi (fun idx el -> (idx, el)) lst
 let fold_lefti fn acc lst =
   List.fold_left (fun acc (idx, el) -> fn acc idx el) acc (zip_with_index lst)
 
+let rec map2_shortest fn lst1 lst2 =
+  match (lst1, lst2) with
+  | head1 :: tail1, head2 :: tail2 ->
+      fn head1 head2 :: map2_shortest fn tail1 tail2
+  | [], _ ->
+      lst2
+  | _, [] ->
+      lst1
+
 let get_neighbor_idxs (row_idx, col_idx) row_length col_length =
   let addends = List.init 3 (fun idx -> idx - 1)
   and is_valid_idx idx bound = idx >= 0 && idx < bound in
---
+++ lib/day04/day04.ml
@@ -0,0 +1,73 @@
+let map_to_num_of_wins cards =
+  let pattern = Str.regexp {|^Card +[0-9]+: +\(.+\) | \(.+\)$|} in
+  cards
+  |> List.map (fun line ->
+         if Str.string_match pattern line 0 then (
+           let winning_nums = Hashtbl.create (List.length cards) in
+           Str.matched_group 1 line |> String.split_on_char ' '
+           |> List.iter (fun num ->
+                  if num <> "" then Hashtbl.add winning_nums num () ) ;
+           let our_nums =
+             Str.matched_group 2 line |> String.split_on_char ' '
+             |> List.filter (fun substr -> substr <> "")
+           in
+           let count =
+             List.fold_left
+               (fun count our_num ->
+                 count + if Hashtbl.mem winning_nums our_num then 1 else 0 )
+               0 our_nums
+           in
+           count )
+         else failwith ("Pattern did not match line " ^ line ^ ".") )
+
+let part_1_aux path =
+  Common.file_to_list path |> map_to_num_of_wins
+  |> List.fold_left
+       (fun sum num_of_wins ->
+         sum
+         +
+         if num_of_wins = 0 then 0
+         else float_of_int 2 ** float_of_int (num_of_wins - 1) |> int_of_float
+         )
+       0
+
+let part_2_aux path =
+  let lines = Common.file_to_list path in
+  lines |> map_to_num_of_wins
+  |> List.fold_left
+       (fun (count, prev) num_of_wins ->
+         match prev with
+         | head :: tail ->
+             ( count + head
+             , Common.map2_shortest
+                 (fun a b -> a + b)
+                 (List.init num_of_wins (fun _ -> head))
+                 tail )
+         | _ ->
+             (count, []) )
+       (0, List.init (List.length lines) (fun _ -> 1))
+  |> fst
+
+let test_1 () = part_1_aux "lib/day04/test.txt"
+
+let part_1 () = part_1_aux "lib/day04/input.txt"
+
+let test_2 () = part_2_aux "lib/day04/test.txt"
+
+let part_2 () = part_2_aux "lib/day04/input.txt"
+
+let test () =
+  print_string "Test part 1: " ;
+  test_1 () |> print_int ;
+  print_newline () ;
+  print_string "Test part 2: " ;
+  test_2 () |> print_int ;
+  print_newline ()
+
+let solution () =
+  print_string "Part 1: " ;
+  part_1 () |> print_int ;
+  print_newline () ;
+  print_string "Part 2: " ;
+  part_2 () |> print_int ;
+  print_newline ()
