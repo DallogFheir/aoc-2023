4fae70e4439b6a60d05eada1e39cad089552c9cd
DallogFheir <dallog.fheir@gmail.com>
2023-12-28 17:09:41 +0100

    day 19 - part 2

--- lib/day19/day19.ml
+++ lib/day19/day19.ml
@@ -131,11 +131,127 @@ let test_1 () =
 
 let part_1 () = part_1_aux "lib/day19/input.txt"
 
+let calculate_all_combinations workflows =
+  let restrict_ranges category value is_first_in_tuple (x, m, a, s) =
+    let restrict_range (range_start, range_end) =
+      let new_range, opposite_range =
+        if is_first_in_tuple then
+          ( (max range_start (value + 1), range_end)
+          , (range_start, min range_end value) )
+        else
+          ( (range_start, min range_end (value - 1))
+          , (max range_start value, range_end) )
+      in
+      ( (if fst new_range > snd new_range then None else Some new_range)
+      , if fst opposite_range > snd opposite_range then None
+        else Some opposite_range )
+    in
+    let restrict_ranges_aux idx =
+      let new_ranges = [|x; m; a; s|] and opposite_ranges = [|x; m; a; s|] in
+      let new_range_opt, opposite_range_opt = restrict_range new_ranges.(idx) in
+      let new_new_ranges =
+        match new_range_opt with
+        | Some new_range ->
+            new_ranges.(idx) <- new_range ;
+            Some new_ranges
+        | _ ->
+            None
+      and new_opposite_ranges =
+        match opposite_range_opt with
+        | Some opposite_range ->
+            opposite_ranges.(idx) <- opposite_range ;
+            Some opposite_ranges
+        | _ ->
+            None
+      in
+      match (new_new_ranges, new_opposite_ranges) with
+      | ( Some [|x_new; m_new; a_new; s_new|]
+        , Some [|x_opposite; m_opposite; a_opposite; s_opposite|] ) ->
+          ( Some (x_new, m_new, a_new, s_new)
+          , Some (x_opposite, m_opposite, a_opposite, s_opposite) )
+      | None, Some [|x_opposite; m_opposite; a_opposite; s_opposite|] ->
+          (None, Some (x_opposite, m_opposite, a_opposite, s_opposite))
+      | Some [|x_new; m_new; a_new; s_new|], None ->
+          (Some (x_new, m_new, a_new, s_new), None)
+      | _ ->
+          failwith "How did this happen?"
+    in
+    match category with
+    | "x" ->
+        restrict_ranges_aux 0
+    | "m" ->
+        restrict_ranges_aux 1
+    | "a" ->
+        restrict_ranges_aux 2
+    | "s" ->
+        restrict_ranges_aux 3
+    | _ ->
+        failwith ("Invalid category: " ^ category)
+  in
+  let rec calculate_all_combinations_aux queue total =
+    match queue with
+    | [] ->
+        total
+    | ((rule, result) :: workflow_tail, ((x, m, a, s) as ranges)) :: tail -> (
+        let restricted_ranges_opt, opposite_ranges_opt =
+          match rule with
+          | LessThan (category, value) ->
+              restrict_ranges category value false ranges
+          | GreaterThan (category, value) ->
+              restrict_ranges category value true ranges
+          | AlwaysTrue ->
+              (Some ranges, None)
+        in
+        if restricted_ranges_opt = None then
+          calculate_all_combinations_aux
+            ((workflow_tail, Utils.option_get opposite_ranges_opt) :: tail)
+            total
+        else
+          let restricted_ranges = Utils.option_get restricted_ranges_opt
+          and next_queue =
+            if opposite_ranges_opt = None then tail
+            else (workflow_tail, Utils.option_get opposite_ranges_opt) :: tail
+          in
+          match result with
+          | Accept ->
+              let x, m, a, s = restricted_ranges in
+              let to_add =
+                [x; m; a; s]
+                |> List.fold_left
+                     (fun prod (range_start, range_end) ->
+                       let range_length = range_end - range_start + 1 in
+                       prod * range_length )
+                     1
+              in
+              calculate_all_combinations_aux next_queue (total + to_add)
+          | Reject ->
+              calculate_all_combinations_aux next_queue total
+          | SendTo other_workflow ->
+              calculate_all_combinations_aux
+                ( (Hashtbl.find workflows other_workflow, restricted_ranges)
+                :: next_queue )
+                total )
+    | _ :: tail ->
+        calculate_all_combinations_aux tail total
+  in
+  calculate_all_combinations_aux
+    [(Hashtbl.find workflows "in", ((1, 4000), (1, 4000), (1, 4000), (1, 4000)))]
+    0
+
+let part_2_aux path =
+  let workflows, _ = parse_input path in
+  calculate_all_combinations workflows
+
+let test_2 () =
+  part_2_aux "lib/day19/test.txt" |> print_int ;
+  print_newline ()
+
+let part_2 () = part_2_aux "lib/day19/input.txt"
+
 let solution () =
   print_string "Part 1: " ;
   part_1 () |> print_int ;
+  print_newline () ;
+  print_string "Part 2: " ;
+  part_2 () |> print_int ;
   print_newline ()
-(* ;
-   print_string "Part 2: " ;
-   part_2 () |> print_int ;
-   print_newline () *)
